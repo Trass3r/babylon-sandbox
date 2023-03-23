cce4abe0f758253f5e107f0c75e2f0d8610115a6
Andreas Hollandt <trass3r@gmail.com>
2023-03-23 11:01:46 +0100

    upgrade everything and make it work again

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,8 +7,6 @@ module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
     devServer: {
-        contentBase: path.resolve(__dirname, "./dist"),
-        compress: true,
-        port: 9000,
-    },
+        static: './'
+    }
 })
